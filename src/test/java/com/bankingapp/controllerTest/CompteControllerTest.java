ba652921cd05eab41575b93dd5a7baccbb4bd057
Stephanie <ishnewalon@hotmail.com>
2022-04-01 14:25:06 -0400

    Added password type to signup

--- src/test/java/com/bankingapp/controllerTest/CompteControllerTest.java
+++ src/test/java/com/bankingapp/controllerTest/CompteControllerTest.java
@@ -12,15 +12,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
